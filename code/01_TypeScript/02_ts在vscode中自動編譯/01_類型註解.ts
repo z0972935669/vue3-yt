f8d0e2e92af0b554449d02b8ab8582ffcc8e7baa
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-10-12 20:03:12 +0800

    update file

---
+++ code/01_TypeScript/02_ts在vscode中自動編譯/01_類型註解.ts
@@ -0,0 +1,13 @@
+// 類型註解: 是一種輕量級的為函數或者變量添加的約束
+
+(()=>{
+    // str是string類型的
+    function showMsg(str:any) {
+        return '床前明月光,'+str
+    }
+    // let msg = '疑是地方霜'
+    // msg是一個數組
+    let msg = [10, 20, 30];
+    // 智能的錯誤提示信息
+    console.log(showMsg(msg));
+})()
